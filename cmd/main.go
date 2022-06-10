61814b75c016ff80c84d8720156ca2d892a72b44
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-06-10 19:19:33 +0100

    created server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Marvellous-Chimaraoke/book-management-system/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+const PORT = ":8080"
+
+// main handles the launching of the server
+func main() {
+	r := mux.NewRouter()
+
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(PORT, r))
+}
