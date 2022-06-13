28c854d77918a3f5f363355214fc4aac0dd1de94
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-06-13 21:16:23 +0100

    wrote GetBook controller for retrieving all books

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,24 @@
 package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/Marvellous-Chimaraoke/book-management-system/pkg/models"
+)
+
+var NewBook models.Book
+
+// GetBook sends http request to retrieve all books.
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, err := json.Marshal(newBooks)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
