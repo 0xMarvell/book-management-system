3919450b5dd88bb73c01658f7e718523985b117d
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-06-13 21:17:59 +0100

    uncommented GetBook route

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,13 +1,15 @@
 package routes
 
 import (
+	"github.com/Marvellous-Chimaraoke/book-management-system/pkg/controllers"
 	"github.com/gorilla/mux"
 )
 
 // RegisterBookSToreRoutes handles routing for the API endpoints
 func RegisterBookStoreRoutes(router *mux.Router) {
 	// router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
-	// router.HandleFunc("/book", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/books", controllers.GetBook).Methods("GET")
+
 	// router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
 	// router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	// router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
