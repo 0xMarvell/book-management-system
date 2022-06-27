8f81439f6482d61bb706ba5297fbb55df322bf4b
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-06-27 22:54:52 +0100

    added port num to env vars

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Marvellous-Chimaraoke/book-management-system/pkg/routes"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	_ "gorm.io/driver/mysql"
 )
-
-const PORT = ":8080"
 
 // main handles the launching of the server
 func main() {
 	r := mux.NewRouter()
 
+	dotEnvErr := godotenv.Load()
+	if dotEnvErr != nil {
+		log.Fatal("error loading envirionment variables")
+	}
+
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
 	fmt.Println("Server is up and running...")
-	log.Fatal(http.ListenAndServe(PORT, r))
+	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), r))
 }
