cf9dcb10304e1053159744ea9e96f21b85576357
0xMarvell <rokemarvellous@gmail.com>
2022-07-09 17:23:32 +0100

    ...

--- pkg/controllers/book-controller_test.go
+++ pkg/controllers/book-controller_test.go
@@ -23,7 +23,7 @@ func TestHomePage(t *testing.T) {
 		}
 
 		if response.StatusCode != http.StatusOK {
-			t.Fatalf("FAILED: expected status code %d ; got %d", http.StatusOK, response.StatusCode)
+			t.Fatalf("FAILED: expected status code %d; got %d", http.StatusOK, response.StatusCode)
 		}
 	})
 
@@ -50,15 +50,15 @@ func TestGetBook(t *testing.T) {
 		httpStatusCode int
 	}{
 		{name: "request with correct endpoint", endpoint: "/books", httpStatusCode: 200},
-		{name: "request with incorrect endpoint", endpoint: "/book/1", httpStatusCode: 404},
+		{name: "request with incorrect endpoint", endpoint: "/books/1", httpStatusCode: 404},
 		{name: "request with spelling error", endpoint: "/boojs", httpStatusCode: 404},
 	}
 
-	for _, tt := range testcases {
-		t.Run(tt.name, func(t *testing.T) {
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
 			testHandler := func() http.Handler {
 				r := http.NewServeMux()
-				r.HandleFunc(tt.endpoint, GetBook)
+				r.HandleFunc(tc.endpoint, GetBook)
 				return r
 			}
 			srv := httptest.NewServer(testHandler())
@@ -69,10 +69,50 @@ func TestGetBook(t *testing.T) {
 				t.Fatalf("could not send GET request: %v", err)
 			}
 
-			if response.StatusCode != tt.httpStatusCode {
-				t.Fatalf("FAILED: expected status code %d ; got %d", tt.httpStatusCode, response.StatusCode)
+			if response.StatusCode != tc.httpStatusCode {
+				t.Fatalf("FAILED: expected status code %d; got %d", tc.httpStatusCode, response.StatusCode)
 			}
 		})
+
 	}
 
 }
+
+// func TestGetBookById(t *testing.T) {
+// 	testcases := []struct {
+// 		name           string
+// 		endpoint       string
+// 		httpStatusCode int
+// 		contentLength  int64
+// 	}{
+// 		{name: "book exists in database", endpoint: fmt.Sprintf("/books/%d", 7), httpStatusCode: 200},
+// 		{name: "book does not exist in database", endpoint: "/books/44", httpStatusCode: 404, contentLength: 74},
+// 		{name: "request with spelling error", endpoint: "/book/7", httpStatusCode: 404, contentLength: 74},
+// 	}
+
+// 	for _, tc := range testcases {
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			testHandler := func() http.Handler {
+// 				r := http.NewServeMux()
+// 				r.HandleFunc(tc.endpoint, GetBookById)
+// 				return r
+// 			}
+
+// 			srv := httptest.NewServer(testHandler())
+// 			defer srv.Close()
+
+// 			response, err := http.Get(fmt.Sprintf("%s/books", srv.URL))
+// 			if err != nil {
+// 				t.Fatalf("could not send GET request: %v", err)
+// 			}
+// 			// fmt.Println(response)
+// 			if response.StatusCode != tc.httpStatusCode {
+// 				t.Fatalf("FAILED: expected status code %d; got %d", tc.httpStatusCode, response.StatusCode)
+// 			}
+
+// 			if response.ContentLength != tc.contentLength {
+// 				t.Fatalf("FAILED: expected response with Content-Length %d; got %d", tc.contentLength, response.ContentLength)
+// 			}
+// 		})
+// 	}
+// }
