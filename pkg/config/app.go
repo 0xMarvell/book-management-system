d1f58ed1c904569003d7652e205de8724b8cb99f
0xMarvell <rokemarvellous@gmail.com>
2022-08-03 01:14:51 +0100

    new LoadEmv func

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -2,37 +2,39 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"regexp"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
-// const projectDirName = "book-management-system" // change to relevant project name
+const projectDirName = "book-management-system" // change to relevant project name
 
-// func loadEnv() {
+/* loadEnv loads data from the .env file using a dynamic path.
+   This helps with preventing errors while loading env files
+   and makes the process of running tests in different subdirectories smooth.
+*/
+func loadEnv() {
 
-// 	/* loadEnv loads data from the .env file using a dynamic path.
-// 	This helps with preventing errors while loading env files
-// 	and makes the process of running tests in different subdirectories smooth.
-// 	*/
+	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	currentWorkDirectory, _ := os.Getwd()
+	rootPath := projectName.Find([]byte(currentWorkDirectory))
 
-// 	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
-// 	currentWorkDirectory, _ := os.Getwd()
-// 	rootPath := projectName.Find([]byte(currentWorkDirectory))
+	err := godotenv.Load(string(rootPath) + `/.env`)
 
-// 	err := godotenv.Load(string(rootPath) + `/.env`)
-
-// 	if err != nil {
-// 		log.Fatalf("Error loading .env file: %v", err)
-// 	}
-// }
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+}
 
 // Connect makes API to connect with MySQL database
 func Connect() {
-	// loadEnv()
+	loadEnv()
 
 	dbUsername := os.Getenv("DBUSERNAME")
 	dbPassword := os.Getenv("DBPASSWORD")
